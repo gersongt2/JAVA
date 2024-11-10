715a549f76b86e0b887484692ee4531ab8e61197
gersongt2 <gersonsilva0387@gmail.com>
2024-11-09 21:27:20 -0300

    t

---
+++ HelloWorld/meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        String nome = s.next();
+        System.out.println("Hello!! "+nome);
+    }
+}
